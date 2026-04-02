fc7aa658f0a2122ecd77564f11dac0abd402fa07
Norman Masià <norman71@uoc.edu>
2026-04-02 17:48:42 +0200

    Example de biax i variància

--- src/python-proves/Exemple_de_Biaix_i_Variància.py
+++ src/python-proves/Exemple_de_Biaix_i_Variància.py
@@ -1,10 +1,12 @@
 # Exemple de Biaix i Variància
 
-# Importar la llibreria
+# Importar les llibreries
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Matriu de dades
-punts = np.array([[1.0, 1.9],
+punts = np.array([
+    [1.0, 1.9],
     [1.5, 3.4],
     [2.0, 2.3],
     [2.5, 2.6],
@@ -17,3 +19,31 @@ punts = np.array([[1.0, 1.9],
     [7.0, 4.6]
     ])
 print(punts)
+
+# Separar les dades
+x= punts[:,0]
+y = punts[:,1]
+
+# Crear el coeficients
+coeficients_polinomi = np.polyfit(x, y, 10)
+coeficients_recta = np.polyfit(x, y, 1)
+
+p = np.poly1d(coeficients_polinomi)
+r = np.poly1d(coeficients_recta)
+
+xx = np.linspace(min(x), max(x), 500)
+yy = p(xx)
+yy_recta = r(xx)
+
+
+print('Coeficients polinomi:')
+print(coeficients_polinomi)
+print('Coeficients recta:')
+print(coeficients_recta)
+print('Valors del polinomi:')
+print(p(x))
+plt.scatter(x, y, color="red", label="punts")
+plt.plot(xx, yy, label="polinomi grau 10")
+plt.legend()
+plt.grid(True)
+plt.show()
