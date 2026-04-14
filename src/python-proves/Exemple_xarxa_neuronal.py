db68192f802f51fe7aa1df3bcc25d191c25f032f
Norman Masià <norman71@uoc.edu>
2026-04-14 07:23:37 +0200

    Creació del nou programa sobre xarxes neuronals

---
+++ src/python-proves/Exemple_xarxa_neuronal.py
@@ -0,0 +1,4 @@
+# Arxiu per explorar les diferents configuracions possibles en l'entrenament d'una xarxa neuronal
+# Es treballarà amb les dades bàsiques de iris.csv
+# Part del codi, és original dels exercicis de la UOC.
+
